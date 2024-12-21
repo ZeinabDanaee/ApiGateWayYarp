9c04955e8372f93638597da6cee9de6cef0412fa
danaei <danaee.web@gmail.com>
2024-12-21 12:59:00 +0330

    Use ApiGateWay

---
+++ Controllers/Service1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace service1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Service1 : ControllerBase
+    {
+        [HttpGet("/Service1")]
+        public string service1()
+        {
+            return "service1";
+        }
+    }
+}
